a2fe3abd320c5b266991f3e036bf78278fec1ff6
7ayushgupta <ayushgup@iitk.ac.in>
2018-02-04 00:31:42 +0530

    Simple broadcasting using Sockets and Tkinter

---
+++ intro.py
@@ -0,0 +1,6 @@
+Websockets are one of the coolest technologies in recent years. They are getting popular mostly because they allow two-way communication between server and browser. In traditional HTTP application client sends requests and server issues response after which their exchange is terminated. This model is totally okay for most web apps, but it is inefficient for applications that require realtime communication. RFC 6455 is probably most detailed introduction to websockets specs.
+
+
+Doubts:
+What to do about asynchronous communications?  Onyl when both of them are online, will I be able to send messages.
+
